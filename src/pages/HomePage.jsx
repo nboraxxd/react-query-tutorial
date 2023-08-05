1b8133828f902efb2c2a45cc730f39c21db67f75
bora <nhatbaoxxd@gmail.com>
2023-08-05 15:55:10 +0700

    queryClient

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,27 @@
+import { useQueryClient } from '@tanstack/react-query'
 import React from 'react'
 const HomePage = () => {
+  const queryClient = useQueryClient()
+  const queryKeys = [
+    ['posts', 2],
+    ['posts', 4],
+  ]
+
+  const data = queryKeys.map((queryKey) => queryClient.getQueryData(queryKey))
+  console.log('🔥 ~ HomePage ~ queryClient:', data)
+
+  const handleClearCache = () => {
+    const arrClear = [
+      ['posts', 2],
+      ['posts', 3],
+    ]
+    arrClear.forEach((queryKey) => queryClient.removeQueries(queryKey))
+  }
+
   return (
     <div>
       <h1>HomePage</h1>
+      <button onClick={handleClearCache}>Clear cache</button>
     </div>
   )
 }
